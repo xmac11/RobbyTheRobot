f6ed433e3e4a6d57f07b781950397ce42be7edb7
Charalampos Makrylakis <cxm881@cs.bham.ac.uk>
2019-06-27 16:32:30 +0100

    Initial commit

---
+++ desktop/src/com/robot/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.robot.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.robot.game.RobotGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RobotGame(), config);
+	}
+}
