49b90946a57aaba32d516480e7c84259c4b9acc4
Charalampos Makrylakis <cxm881@cs.bham.ac.uk>
2019-06-29 19:49:13 +0100

    Add pipe_water assets

---
+++ android/assets/pipe_water.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="pipe_water" tilewidth="16" tileheight="16" tilecount="24" columns="6">
+ <image source="pipe_water.png" width="96" height="64"/>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="16" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <animation>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="22" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
