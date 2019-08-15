4124f47011c50a1a2433f5f6f9bd32991161c0e5
Charalampos Makrylakis <cxm881@cs.bham.ac.uk>
2019-08-15 04:03:48 +0100

    Added interpolation and delay to LostLifeScreen

--- core/src/com/robot/game/screens/LostLifeScreen.java
+++ core/src/com/robot/game/screens/LostLifeScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
@@ -78,10 +79,13 @@ public class LostLifeScreen extends ScreenAdapter {
         });
 
         SequenceAction sequenceAction = new SequenceAction();
-        sequenceAction.addAction(Actions.fadeOut(1f));
+
+        sequenceAction.addAction(Actions.fadeOut(1f, Interpolation.fade));
         sequenceAction.addAction(changeText);
-        sequenceAction.addAction(Actions.fadeIn(1f));
+        sequenceAction.addAction(Actions.fadeIn(1f, Interpolation.fade));
+        sequenceAction.addAction(Actions.delay(0.25f));
         sequenceAction.addAction(respawn);
+
         initialLives.addAction(sequenceAction);
     }
 
